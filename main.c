55d95a08542a6b014f9175496d99b8a86d822c42
Tyler <mclemoret0@students.rowan.edu>
2020-11-24 18:05:14 -0500

    Added skeleton code to main

--- main.c
+++ main.c
@@ -8,5 +8,31 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	//--Initial Setup--
+
+
+	if(Initial_Boot){//check to see if this is the first time the device has booted up
+	    //Go to Initial boot sequence
+	    //Remember, we must wait 15 minutes before broadcasts
+
+	}else{
+	    //Find out what went wrong and resume normal operations
+	    //Send a message to ground control that the system just rebooted and is back online
+	}
+
+
 	return 0;
+}
+
+/*
+ * Checks to see if this is the first time the device has booted up
+ * Or if this is a reboot from a previous failed state/power loss
+ *
+ * In the event that the program can not determine if this is
+ * an initial boot, it should assume that it is an initial boot
+ *
+ */
+bool Initial_Boot(){
+
+    return true;
 }
